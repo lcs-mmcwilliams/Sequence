b75ee46de497bceccd5e648a2bc589bbeb0919af
Max McWilliams <mmcwilliams@lcs-mba3147.lcs.on.ca>
2017-09-30 12:02:21 -0400

    progress

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -22,10 +22,32 @@ import PlaygroundSupport
 
 // Create a new canvas
 let canvas = Canvas(width: 600, height: 500)
+
 // Create a blue background
 canvas.borderColor = Color.blue
 canvas.fillColor = Color.init(hue: 200, saturation: 50, brightness: 100, alpha: 100)
 canvas.drawRectangle(centreX: 0, centreY: 0, width: 10000, height: 1200)
+
+canvas.drawAxes()
+
+//rays
+canvas.defaultLineWidth = 5
+canvas.lineColor = Color.yellow
+canvas.borderColor = Color.orange
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 500)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 400)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 300)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 200)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 100)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 600, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 400, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 300, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 400, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 300, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 200, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 100, toY: 0)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 0, toY: 0)
 
 //create circle for sun
 canvas.fillColor = Color.yellow
